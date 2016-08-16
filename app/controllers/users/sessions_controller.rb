44488f98db4b26f20c45a9beced05016dd57480d
emiliocorpus <emiliocorpus@gmail.com>
2016-08-16 16:50:07 -0700

    updates devise controller and routes with navbar sign in link

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,23 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    binding.pry
+    user = User.find_by_email(params['email'])
+    if user
+      if user.password == params["password"]
+
+      end
+    else
+      redirect_to root_path
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'page#index'
+
+  devise_scope :user do
+    post 'sign_in', to: 'users/sessions#create', as: 'sign_in'
+    delete 'sign_out', to: 'users/sessions#destroy', as: 'sign_out'
+  end
 
 end
