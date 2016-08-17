b028aa2980ea938a8aef60c7d0ade94d70a18870
emiliocorpus <emiliocorpus@gmail.com>
2016-08-16 18:57:11 -0700

    adds devise user model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816211759) do
+ActiveRecord::Schema.define(version: 20160817015609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
