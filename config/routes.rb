227f299c4d1f739d84206f520a04b3bc63a13bd8
emiliocorpus <emiliocorpus@gmail.com>
2016-08-16 19:31:29 -0700

    configures devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'page#index'
 
-
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 end
