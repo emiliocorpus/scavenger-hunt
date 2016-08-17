bdd30825cc963a1a782afd4c0b8e20bfeeed66d1
emiliocorpus <emiliocorpus@gmail.com>
2016-08-16 20:28:44 -0700

    initialize show path

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,16 @@
 class PageController < ApplicationController
   
   def index
+  	if current_user
+  		redirect_to user_profile_path(current_user.id)
+  	end
   end
 
   def show
   	
+  	if current_user
+  		@user = User.find_by_email(current_user.email)
+  	end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'page#index'
-
+  get 'users/:id' => 'page#show', as: 'user_profile'
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 end
